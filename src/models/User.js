c0825cbbd0501333da18580d35fb0fb162fa86a6
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-08-01 21:12:35 -0300

    CREATE:MODEL

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id:{
+            primaryKey: true,
+            autoIncrement:true,
+            type: DataTypes.INTERGER,
+        },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+      },{
+        tableName: 'users',
+        timestamps: false,
+        underscored: true,
+      });
+
+      return User;
+};
