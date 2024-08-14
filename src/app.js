641a39b022ed9a43f9fd962bec201c1a7160adf3
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-08-13 21:19:14 -0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
-  response.send('hello world');
+  response.send('');
 });
 
 app.use(express.json());
